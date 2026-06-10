e7395bce0d2e091ec8a37733dcae01f3f3bc11d9
unknown <midnightsun1315-cyber>
2026-06-10 16:40:30 +0530

    DAY-10 Assignment

---
+++ DAY-10/question3d10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class question3d10 {
+  public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<= n-i ;j++){
+                System.out.print(" ");
+               
+                }
+                for (int k=1;k<=i;k++){
+                    System.out.print( k);
+                }
+                for (int m=i-1;m>=1;m--){
+                    System.out.print(m);
+                }
+                 System.out.println();
+            }
+          
+            sc.close();
+        }
+
+       
+    }
+    
+
+
+    
+
---
+++ DAY-10/question4d10.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class question4d10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<= n-i ;j++){
+                System.out.print(" ");
+               
+                }
+                for (int k=1;k<=i;k++){
+                    System.out.print( (char) (k +64));
+                }
+                for (int m=i-1;m>=1;m--){
+                    System.out.print( (char) (m +64));
+                }
+                 System.out.println();
+            }
+          
+            sc.close();
+        }
+
+       
+    }
+    
+
+
+    
+
+
+    
